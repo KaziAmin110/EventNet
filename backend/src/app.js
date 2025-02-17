b2a2ade7682493e2f29d3c1a561860002e7223d9
Kazi Amin <126922373+KaziAmin110@users.noreply.github.com>
2025-02-17 12:38:54 -0500

    Reorganized backend files and folders

---
+++ backend/src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import { PORT } from "../config/env.js";
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+
+// Routes
+
+
+// Test Route
+app.get("/", (req, res) => res.send("Backend is running"));
+
+// // Example Route
+// import userRoutes from "./routes/userRoutes.js";
+// app.use("/users", userRoutes);
+
+app.listen(PORT, async () => {
+    console.log(
+        `Events Tracker API is running on http://localhost:${PORT}`
+    );
+});
