5a0815640b09d9b406bdce0600014a5694087ef0
EdwardR2023 <edwardr@knights.ucf.edu>
2025-03-17 19:27:15 -0400

    sign up and sign in functionality added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,9 @@
 export default {
-    server: {
-      proxy: {
-        "/api": "http://localhost:5000",
-      },
+  envDir: '../backend', // points to the directory containing your .env.development.local file
+  envPrefix: ['SUPABASE_', 'VITE_'], // expose variables starting with SUPABASE_ and VITE_
+  server: {
+    proxy: {
+      "/api": "http://localhost:5000",
     },
-  };
-  
+  },
+};
