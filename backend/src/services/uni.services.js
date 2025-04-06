61fc33dc33c72ec36626a87e6d8006878671a7ad
Kazi Amin <126922373+KaziAmin110@users.noreply.github.com>
2025-04-06 18:01:55 -0400

    Fixed Get Joinable Universities Error

--- backend/src/services/uni.services.js
+++ backend/src/services/uni.services.js
@@ -452,33 +452,18 @@ export const getJoinableUniversitiesDB = async (
   end
 ) => {
   try {
-    // // Generate cache key based on user_id
-    const cacheKey = `joinable_unis:${user_id}`;
-
-    // // Check if the user RSO data is cached in Redis
-    const cachedRsos = await redisClient.get(cacheKey);
-    if (cachedRsos !== null) {
-      return JSON.parse(cachedRsos); // Return cached data
-    }
-
     // Fetch data from Supabase if not found in cache
-    const { data, error, count } = await supabase
-      .rpc(
-        "get_joinable_universities",
-        {
-          user_id_input: user_id,
-          user_domain_input: user_email.split("@")[1],
-        },
-        { count: "exact" }
-      )
-      .range(start, end);
+    const { data, error, count } = await supabase.rpc(
+      "get_joinable_universities",
+      {
+        user_id_input: user_id,
+        user_domain_input: user_email.split("@")[1],
+      }
+    );
 
     if (error) {
       throw new Error(error.message);
     }
-    // // Cache the RSOs data in Redis with 5 minutes expiration
-    await redisClient.set(cacheKey, JSON.stringify(data), "EX", 300); // 5 minutes expiration
-
     return {
       data: data || [],
       count: count,
